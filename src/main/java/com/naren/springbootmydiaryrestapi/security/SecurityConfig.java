e697e2fd901e0ed8888ae43cf350b7f0e7c75bbd
narender <narendarmekala.9778@gmail.com>
2024-07-25 21:42:50 +0530

    Role Based Authorization in Spring Security

--- src/main/java/com/naren/springbootmydiaryrestapi/security/SecurityConfig.java
+++ src/main/java/com/naren/springbootmydiaryrestapi/security/SecurityConfig.java
@@ -4,13 +4,19 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	
 	
@@ -35,6 +41,32 @@ public class SecurityConfig {
 		return inMemoryUserDetailsManager;
 	}*/
 	
+	@Bean
+	public SecurityFilterChain authorizeHttpRequests(HttpSecurity http)throws Exception{
+		
+		http
+		.authorizeHttpRequests(
+				(authorize)->{
+					
+					authorize
+					.requestMatchers(HttpMethod.DELETE,"/entries/**").hasAuthority("ROLE_ADMIN")
+					.requestMatchers(HttpMethod.PUT,"/entries/**").hasAuthority("ROLE_MANAGER")
+					.anyRequest().authenticated();
+					
+				}
+				
+				)
+		.httpBasic()
+		.and()
+		.csrf().disable();
+		return http.build();
+	}
 	
 
 }
+
+
+
+
+
+
